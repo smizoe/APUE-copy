51e2da922a859568fe7e7134a45d275f26618aa4
Sho Mizoe <sho.mizoe@gmail.com>
2015-12-27 09:33:20 -0800

    use the latest error.c and errorlog.c

--- include/errorlog.c
+++ include/errorlog.c
@@ -1,81 +1,124 @@
-#include <errno.h>
-#include <stdarg.h>
+/*
+ * Error routines for programs that can run as a daemon.
+ */
+
+#include "apue.h"
+#include <errno.h>		/* for definition of errno */
+#include <stdarg.h>		/* ISO C variable arguments */
 #include <syslog.h>
-#include "ourhdr.h"
 
-static void log_doit(int, int, const char *, va_list ap);
+static void	log_doit(int, int, int, const char *, va_list ap);
 
-extern int debug;
+/*
+ * Caller must define and set this: nonzero if
+ * interactive, zero if daemon
+ */
+extern int	log_to_stderr;
 
-
+/*
+ * Initialize syslog(), if running as daemon.
+ */
 void
 log_open(const char *ident, int option, int facility)
 {
-  if (debug == 0)
-    openlog(ident, option, facility);
+	if (log_to_stderr == 0)
+		openlog(ident, option, facility);
 }
 
+/*
+ * Nonfatal error related to a system call.
+ * Print a message with the system's errno value and return.
+ */
 void
 log_ret(const char *fmt, ...)
 {
-  va_list ap;
+	va_list		ap;
 
-  va_start(ap, fmt);
-  log_doit(1, LOG_ERR, fmt, ap);
-  va_end(ap);
-  return;
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
 }
 
-
+/*
+ * Fatal error related to a system call.
+ * Print a message and terminate.
+ */
 void
 log_sys(const char *fmt, ...)
 {
-  va_list ap;
+	va_list		ap;
 
-  va_start(ap, fmt);
-  log_doit(1, LOG_ERR, fmt, ap);
-  va_end(ap);
-  exit(2);
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
 }
 
+/*
+ * Nonfatal error unrelated to a system call.
+ * Print a message and return.
+ */
 void
-log_msg(const char * fmt, ...)
+log_msg(const char *fmt, ...)
 {
-  va_list ap;
-  
-  va_start(ap, fmt);
-  log_doit(0, LOG_ERR, fmt, ap);
-  va_end(ap);
-  return;
+	va_list		ap;
+
+	va_start(ap, fmt);
+	log_doit(0, 0, LOG_ERR, fmt, ap);
+	va_end(ap);
 }
 
+/*
+ * Fatal error unrelated to a system call.
+ * Print a message and terminate.
+ */
 void
-log_quit(const char * fmt, ...)
+log_quit(const char *fmt, ...)
 {
-  va_list ap;
-  
-  va_start(ap, fmt);
-  log_doit(0, LOG_ERR, fmt, ap);
-  va_end(ap);
-  exit(2);
+	va_list		ap;
+
+	va_start(ap, fmt);
+	log_doit(0, 0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
 }
 
+/*
+ * Fatal error related to a system call.
+ * Error number passed as an explicit parameter.
+ * Print a message and terminate.
+ */
+void
+log_exit(int error, const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	log_doit(1, error, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
+}
+
+/*
+ * Print a message and return to caller.
+ * Caller specifies "errnoflag" and "priority".
+ */
 static void
-log_doit(int errnoflag, int priority, const char *fmt, va_list ap)
+log_doit(int errnoflag, int error, int priority, const char *fmt,
+         va_list ap)
 {
-  int errno_save;
-  char buf[MAXLINE];
+	char	buf[MAXLINE];
 
-  errno_save = errno;
-  vsprintf(buf, fmt, ap);
-  if(errnoflag)
-    sprintf(buf+strlen(buf), ": %s", strerror(errno_save));
-  strcat(buf, "\n");
-  if (debug) {
-    fflush(stdout);
-    fputs(buf, stderr);
-    fflush(stderr);
-  } else
-    syslog(priority, buf);
-  return;
+	vsnprintf(buf, MAXLINE-1, fmt, ap);
+	if (errnoflag)
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s",
+		  strerror(error));
+	strcat(buf, "\n");
+	if (log_to_stderr) {
+		fflush(stdout);
+		fputs(buf, stderr);
+		fflush(stderr);
+	} else {
+		syslog(priority, "%s", buf);
+	}
 }
